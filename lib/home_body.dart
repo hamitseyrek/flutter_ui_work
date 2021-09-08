48327ecb35f33ab29745317639e4fdfbfcd68689
hamitseyrek <hmtsyrk@gmail.com>
2021-09-08 16:42:49 +0300

    create complex Card widget

--- lib/home_body.dart
+++ lib/home_body.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeBody extends StatefulWidget {
@@ -98,23 +99,168 @@ class _HomeBodyState extends State<HomeBody> {
         borderRadius: BorderRadius.circular(16),
         elevation: 6,
         child: SizedBox(
-          height: 400,
+          height: 550,
           width: double.infinity,
           child: Column(
-            children: const [
-              SizedBox(
+            children: [
+              const SizedBox(
                 height: 50,
                 width: double.infinity,
                 child: ListTile(
                   leading: CircleAvatar(
                     backgroundImage: AssetImage('assets/model1.jpeg'),
-                    radius: 35,
+                    radius: 25,
                   ),
-                  title: Text('Daisy'),
-                  subtitle: Text('34 min ago'),
-                  trailing: Icon(Icons.architecture),
+                  title: Text(
+                    'Daisy',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, fontFamily: 'Montserrat'),
+                  ),
+                  subtitle: Text(
+                    '34 min ago',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, fontFamily: 'Montserrat'),
+                  ),
+                  trailing: Icon(Icons.more_vert),
                 ),
               ),
+              const SizedBox(height: 15),
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: const Text(
+                  'To add custom fonts to your application, add a fonts section here, in this flutter section. Each entry in this list should have a family key with the font family name, and a fonts key with a',
+                  style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      color: Colors.grey,
+                      fontSize: 13),
+                ),
+              ),
+              const SizedBox(height: 15),
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(left: 10),
+                    height: 210,
+                    width: (MediaQuery.of(context).size.width - 50) / 2,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        image: const DecorationImage(
+                            image: AssetImage('assets/modelgrid1.jpeg'),
+                            fit: BoxFit.cover)),
+                  ),
+                  const SizedBox(width: 15),
+                  Column(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: (MediaQuery.of(context).size.width - 100) / 2,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            image: const DecorationImage(
+                                image: AssetImage('assets/modelgrid2.jpeg'),
+                                fit: BoxFit.cover)),
+                      ),
+                      const SizedBox(height: 10),
+                      Container(
+                        height: 100,
+                        width: (MediaQuery.of(context).size.width - 100) / 2,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            image: const DecorationImage(
+                                image: AssetImage('assets/modelgrid3.jpeg'),
+                                fit: BoxFit.cover)),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.brown.withOpacity(0.01)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        '# Louis vuitton',
+                        style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 10,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.brown.withOpacity(0.01)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        '# Chloe',
+                        style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 10,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Divider(),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Row(
+                  children: [
+                    Icon(Icons.reply,
+                        color: Colors.brown.withOpacity(0.3), size: 30),
+                    const SizedBox(width: 3),
+                    const Text('1.7k',
+                        style: TextStyle(fontFamily: 'Montserrat')),
+                    const SizedBox(width: 20),
+                    Icon(Icons.comment,
+                        color: Colors.brown.withOpacity(0.3), size: 30),
+                    const SizedBox(width: 3),
+                    const Text('216',
+                        style: TextStyle(fontFamily: 'Montserrat')),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: const [
+                        Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                          size: 30,
+                        ),
+                        SizedBox(width: 3),
+                        Text(
+                          '216',
+                          style: TextStyle(fontFamily: 'Montserrat'),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
